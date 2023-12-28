9abc571e9f359bab0ee4f646200e0bc596f23d8c
Swapnil Opash <sra.opash@gmail.com>
2023-12-28 17:50:42 +0530

    fixed bucket name

--- src/helpers/aws-s3.helper.js
+++ src/helpers/aws-s3.helper.js
@@ -13,7 +13,7 @@ exports.uploadFileToWasabi = async (file, key) => {
       fs.readFile(file.path, function (err, buffer) {
         if (err) throw err; // Something went wrong!
         const params = {
-          Bucket: "christian-team",
+          Bucket: "legal-talk",
           Key: key,
           Body: buffer,
         };
